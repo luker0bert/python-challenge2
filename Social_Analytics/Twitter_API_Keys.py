625c59856b40c16fa71c6a47af8a123771a03fd2
Luke Williams <luke11williams@gmail.com>
2018-08-27 10:27:40 -0400

    added multiple files

---
+++ Social_Analytics/Twitter_API_Keys.py
@@ -0,0 +1,6 @@
+# Twitter API keys
+
+consumer_key = "r88UzgUheSOF3pMyINIT5yITY"
+consumer_secret = "kq3NV0k5Dlq4uCucw5Nf0sJAuP09l0Xja1pNc1DenZoxWSKwxk"
+access_token = "421125873-OuCvIPTHHMqC9t8uG8CTSBLi7iK4NNEyZeDQMc9w"
+access_token_secret = "EC2ookZjWBWKo5MpCoEEtrg2OGiF8WZZ1LCiv4nxl45tH"
